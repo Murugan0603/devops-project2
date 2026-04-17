25abb32f64d0f139b72d34abbcaff0546cb0827c
Murugan <manokm98@gmail.com>
2026-04-17 12:28:45 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "devops_server" {
   ami           = "ami-0f5ee92e2d63afc18"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "DevOps-Server"
